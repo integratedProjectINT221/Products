e9c93c4c866d2dbe038c7ce6ed8553d801076ac1
Kampol Thaipricha <kampol.43@mail.kmutt.ac.th>
2021-05-05 18:02:36 +0700

    fixing responsive

--- src/views/Product_Colletions.vue
+++ src/views/Product_Colletions.vue
@@ -1,54 +1,76 @@
 <template>
-  <div>
-    <p
-      id="header"
-      class="text-2xl font-semibold text-center p-10"
-      v-if="this.brand !== null"
-    >
-      {{ brand.brandName }}
-    </p>
-    <div class="mb-4">
-      <div class="inline-flex space-x-2 ml-116">
-        <router-link to="/all_product_brands">All Brands</router-link><span>></span>
-        <router-link :to="{path: `/product_collections/${brand.brandId}`}">{{this.brand.brandName}}</router-link>
-      </div>
+  <p
+    id="header"
+    class="text-2xl font-semibold text-center p-10"
+    v-if="this.brand !== null"
+  >
+    {{ brand.brandName }}
+  </p>
+  <!-- <div class="w-screen h-auto flex justify-center">
+    <div class="inline-flex space-x-2">
+      <router-link to="/all_product_brands">All Brands</router-link
+      ><span>></span>
+      <router-link :to="{ path: `/product_collections/${brand.brandId}` }">{{
+        this.brand.brandName
+      }}</router-link>
     </div>
-    <div id="brands-page" class="w-screen h-auto flex justify-center">
-    <div id="contrainer-brands" class="absolute left-28 space-y-2">
+  </div> -->
+  <div id="brands-page" class="md:w-screen h-auto flex flex-row space-x-12 justify-center bg-green-200 w-160">
+    <div class="flex flex-row md:mr-64 mr-18"> 
+    <div id="contrainer-brands" class=" space-y-2 bg-blue-200 md:ml-20">
       <p class="text-lg font-semibold pb-1">Brands</p>
       <div v-for="brand in brands" :key="brand.brandId" id="loopbrands">
-        <router-link  @click="reloadPage(brand)"  :to="{path: `/product_collections/${brand.brandId}`}">
-            <div class="text-base cursor-pointer">
-              {{ brand.brandName }} 
-            </div>
+        <router-link
+          @click="reloadPage(brand)"
+          :to="{ path: `/product_collections/${brand.brandId}` }"
+        >
+          <div class="text-base cursor-pointer">
+            {{ brand.brandName }}
+          </div>
         </router-link>
       </div>
-    </div>
-      <div id="contrainer-brands" class="grid grid-cols-3 gap-4">
+    </div> 
+    
+    <div class="flex flex-col bg-pink-200 md:ml-28 ml-14  md:w-160">
+    <div class="inline-flex space-x-2 ">
+      <router-link to="/all_product_brands">All Brands</router-link
+      ><span>></span>
+      <router-link :to="{ path: `/product_collections/${brand.brandId}` }">{{
+        this.brand.brandName
+      }}</router-link>
+      </div>
+    <div id="contrainer-brands" class="grid md:grid-cols-3 grid-cols-1 md:gap-4 ">
+      
+      <div
+        v-for="product in productsByBrand"
+        :key="product.prodId"
+        id="loopCollection"
+      >
+        <router-link :to="{ path: `/show/${product.prodId}` }">
+          <img
+            :src="`http://localhost:8081/files/${product.image}`"
+            class="bg-gray-300 md:h-60 md:w-48 w-80 h-96 flex justify-center items-center group cursor-pointer object-cover"
+          />
+        </router-link>
         <div
-          v-for="product in productsByBrand"
-          :key="product.prodId"
-          id="loopCollection"
+          id="container-colors"
+          class="w-48 grid grid-cols-7 gap-y-3 gap-x- mt-2"
         >
-
-          <router-link :to="{ path: `/show/${product.prodId}` }">
-            <img :src="`http://localhost:8081/files/${product.image}`"
-              class="bg-gray-300 h-60 w-48 flex justify-center items-center group cursor-pointer object-cover">
-          </router-link>
-          <div id="container-colors" class="w-48 grid grid-cols-7 gap-y-3 gap-x- mt-2">
-            <div
-              v-for="color in product.colors"
-              :key="color.colorId"
-               class="flex justify-center items-center w-5 h-5 rounded-full"
-              :class="blackBorder(color.colorId)"
-              :style="{ backgroundColor: color.colorId }"
-            ></div>
-          </div>
-          <p class="font-medium">{{ product.prodName }}</p>
-          <p class="font-base">{{ product.price }} THB</p>
+          <div
+            v-for="color in product.colors"
+            :key="color.colorId"
+            class="flex justify-center items-center w-5 h-5 rounded-full"
+            :class="blackBorder(color.colorId)"
+            :style="{ backgroundColor: color.colorId }"
+          ></div>
         </div>
+        <p class="font-medium">{{ product.prodName }}</p>
+        <p class="font-base">{{ product.price }} THB</p>
       </div>
     </div>
+    </div>
+  </div>
+  
   </div>
 </template>
 <script>
@@ -60,20 +82,20 @@ export default {
   // },
   data() {
     return {
-      url: 'http://localhost:8081',
+      url: "http://localhost:8081",
       productsByBrand: [],
       brand: {},
       brands: [],
     };
   },
   methods: {
-    reloadPage(brand){
-      location.assign(`/product_collections/${brand.brandId}`)
-      console.log("reload!")
+    reloadPage(brand) {
+      location.assign(`/product_collections/${brand.brandId}`);
+      console.log("reload!");
     },
-    blackBorder(colorId){
-      if(colorId === '#FFFFFF'){
-        return 'border border-gray-400 opacity-80'
+    blackBorder(colorId) {
+      if (colorId === "#FFFFFF") {
+        return "border border-gray-400 opacity-80";
       }
     },
     async getBrands() {
@@ -90,7 +112,7 @@ export default {
         const res = await fetch(
           `${this.url}/productsByBrand/${this.$route.params.brandId}`
         );
-        console.log(res)
+        console.log(res);
         const data = await res.json();
         return data;
       } catch (error) {
