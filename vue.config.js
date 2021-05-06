7be287b2550913e61181e2bf97485c094fd50e28
Jirawath <Jirawath.jiw@mail.kmutt.ac.th>
2021-05-06 18:37:38 +0700

    vue reverse proxy

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,24 @@
 module.exports = {
     devServer: {
-      proxy: {
-        '^/products': {
-          target: 'http://localhost:8081/',
-          ws: true,
-          changeOrigin: true
-        },
-      }
+        proxy: {
+            '^/products': {
+                target: 'http://104.215.183.81:8081/',
+                ws: true,
+                changeOrigin: true,
+                logLevel: debug
+            },
+            '^/brands': {
+                target: 'http://104.215.183.81:8081/',
+                ws: true,
+                changeOrigin: true,
+                logLevel: debug
+            },
+            '^/colors': {
+                target: 'http://104.215.183.81:8081/',
+                ws: true,
+                changeOrigin: true,
+                logLevel: debug
+            },
+        }
     }
-  }
+}
