2401cecd3a2ee3e3bacaf20efda94b6c598f709c
Jirawath <Jirawath.jiw@mail.kmutt.ac.th>
2021-05-06 19:46:43 +0700

    vue reverse proxy

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,5 @@
 module.exports = {
     devServer: {
-        proxy: {
-            '^/products': {
-                target: 'https://reqres.in/',
-                ws: true,
-                changeOrigin: true,
-                logLevel: 'debug'
-            },
-            '^/brands': {
-                target: 'http://104.215.183.81:8081/',
-                ws: true,
-                changeOrigin: true,
-                logLevel: 'debug'
-            },
-            '^/colors': {
-                target: 'http://104.215.183.81:8081/',
-                ws: true,
-                changeOrigin: true,
-                logLevel: 'debug'
-            },
-        }
+        proxy: 'http://104.215.183.81:8081/'
     }
 }
