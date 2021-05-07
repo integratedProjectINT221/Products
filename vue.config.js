63e51196bbcd9db98e216946c7ec24a3619aa36e
Jirawath <Jirawath.jiw@mail.kmutt.ac.th>
2021-05-07 21:36:13 +0700

    proxy vue

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,30 @@
 module.exports = {
     devServer: {
-        proxy: 'http://104.215.183.81:8081/'
+        proxy: {
+            '^/products': {
+                target: 'http://104.215.183.81:8081/',
+                changeOrigin: true,
+                logLevel: "debug",
+                pathRewrite: {
+                    "^/products": "/"
+                }
+            },
+            '^/brands': {
+                target: 'http://104.215.183.81:8081/',
+                changeOrigin: true,
+                logLevel: "debug",
+                pathRewrite: {
+                    "^/brands": "/"
+                }
+            },
+            '^/colors': {
+                target: 'http://104.215.183.81:8081/',
+                changeOrigin: true,
+                logLevel: "debug",
+                pathRewrite: {
+                    "^/colors": "/"
+                }
+            },
+        }
     }
 }
