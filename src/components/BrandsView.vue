f7163b15b6e1384b4043ffc21039455435075661
Jirawath <Jirawath.jiw@mail.kmutt.ac.th>
2021-05-10 02:25:54 +0700

    [Test] On Production

--- src/components/BrandsView.vue
+++ src/components/BrandsView.vue
@@ -1,20 +1,21 @@
 <template>
-  <div id="brands-page" class="w-screen flex justify-center">
+  <div id="brands-page" class="md:w-screen w-160 flex justify-center">
     <div
       id="contrainer-brands"
-      class="grid grid-cols-4 gap-1 overflow-scroll overflow-x-hidden"
+      class="grid md:grid-cols-4 grid-cols-2 gap-1  md:overflow-scroll md:overflow-x-hidden w-96 md:w-max"
       style="height: 30rem"
     >
-      <div v-for="brand in brands" :key="brand.id" id="loopbrands">
-        <router-link to="/ProductsbyBrand/">
-          <div
-            class="bg-gray-300 h-40 w-40 hover:bg-gray-400 flex justify-center items-center group transition duration-100 cursor-pointer"
+      <div v-for="brand in brands" :key="brand.brandId" id="loopbrands">
+        <router-link  :to="{path: `/ProductsbyBrand/${brand.brandId}`}">
+          <div 
+            class="bg-gray-300 md:h-40 h-44 md:w-40 hover:bg-gray-400 flex justify-center items-center group transition duration-100 cursor-pointer"
           >
-            <div
-              class="text-gray-800 text-2xl font-medium group-hover:text-white transition duration-75 cursor-pointer"
+            <p
+              class="text-gray-800 text-xl font-medium group-hover:text-white transition duration-75 cursor-pointer break-words text-center"
             >
               {{ brand.brandName }}
-            </div>
+              
+            </p>
           </div>
         </router-link>
       </div>
--- src/views/Brands.vue
+++ src/views/Brands.vue
@@ -1,30 +1,13 @@
 <template>
   <p id="header" class="text-2xl text-center p-10">Brands</p>
-  <div id="brands-page" class="md:w-screen w-160 flex justify-center">
-    <div
-      id="contrainer-brands"
-      class="grid md:grid-cols-4 grid-cols-2 gap-1  md:overflow-scroll md:overflow-x-hidden w-96 md:w-max"
-      style="height: 30rem"
-    >
-      <div v-for="brand in brands" :key="brand.brandId" id="loopbrands">
-        <router-link  :to="{path: `/ProductsbyBrand/${brand.brandId}`}">
-          <div 
-            class="bg-gray-300 md:h-40 h-44 md:w-40 hover:bg-gray-400 flex justify-center items-center group transition duration-100 cursor-pointer"
-          >
-            <p
-              class="text-gray-800 text-xl font-medium group-hover:text-white transition duration-75 cursor-pointer break-words text-center"
-            >
-              {{ brand.brandName }}
-              
-            </p>
-          </div>
-        </router-link>
-      </div>
-    </div>
-  </div>
+  <BrandsView :brands="brands"/>
 </template>
 <script>
+import BrandsView from "@/components/BrandsView"
 export default {
+  components:{
+    BrandsView
+  },
   name: "Brands",
   data() {
     return {
